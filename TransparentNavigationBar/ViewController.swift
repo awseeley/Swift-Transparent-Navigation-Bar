b58a23ff6e9a69b55c3f274e49c3ecc2fe702af8
Andrew Seeley <awseeley@gmail.com>
2016-02-21 10:52:25 +1100

    Swift Transparent Navigation Bar
    Swift Transparent Navigation Bar Tutorial

--- TransparentNavigationBar/ViewController.swift
+++ TransparentNavigationBar/ViewController.swift
@@ -13,6 +13,21 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        /*
+        // Transparent navigation bar
+        self.navigationController?.navigationBar.setBackgroundImage(UIImage(), forBarMetrics: UIBarMetrics.Default)
+        self.navigationController?.navigationBar.shadowImage = UIImage()
+        self.navigationController?.navigationBar.translucent = true
+        */
+        
+        /* Use this to set it back to default */
+        
+        self.navigationController?.navigationBar.setBackgroundImage(nil, forBarMetrics: UIBarMetrics.Default)
+        self.navigationController?.navigationBar.shadowImage = nil
+        self.navigationController?.navigationBar.tintColor = nil
+        self.navigationController?.navigationBar.translucent = false
+
     }
 
     override func didReceiveMemoryWarning() {
